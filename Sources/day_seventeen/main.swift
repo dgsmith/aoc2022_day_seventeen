74bdb75daa6d92c1f681a3954946848eff00791b
Grayson Smith <d.grayson.smith@gmail.com>
2022-12-17 15:20:44 -0800

    pt 1

--- Sources/day_seventeen/main.swift
+++ Sources/day_seventeen/main.swift
@@ -1,6 +1,8 @@
 import Darwin
 import Foundation
 
+let log = false
+
 var movements = [AirMovement]()
 
 let filePath = "/Users/grayson/code/advent_of_code/2022/day_seventeen/test.txt"
@@ -16,11 +18,14 @@ var lineCap: Int = 0
 while getline(&lineByteArrayPointer, &lineCap, filePointer) > 0 {
     let line = Array(String(cString:lineByteArrayPointer!))
     for elem in line {
+        if elem == "\n" {
+            break
+        }
         movements.append(AirMovement(character: elem))
     }
 }
 
-enum AirMovement {
+enum AirMovement: String {
     case left
     case right
     
@@ -35,7 +40,7 @@ enum AirMovement {
     }
 }
 
-class Particle: Hashable {
+class Particle: Hashable, CustomStringConvertible {
     enum Kind {
         case air
         case environment
@@ -44,6 +49,15 @@ class Particle: Hashable {
     
     let id: UUID = UUID()
     let kind: Kind
+    var settled = false
+    
+    var description: String {
+        switch kind {
+        case .air: return "."
+        case .environment: return "^"
+        case .rock: return settled ? "#" : "@"
+        }
+    }
     
     init(kind: Kind) {
         self.kind = kind
@@ -58,9 +72,31 @@ class Particle: Hashable {
     }
 }
 
-class Shape {
+class Shape: CustomStringConvertible {
+    enum Kind {
+        case line
+        case plus
+        case reverseL
+        case i
+        case block
+    }
+    
+    let name: String
     let particles: [[Particle]]
-    var settled: Bool = false
+    var settled: Bool = false {
+        didSet {
+            guard settled else {
+                return
+            }
+            for row in particles {
+                for particle in row {
+                    if particle.kind == .rock {
+                        particle.settled = settled
+                    }
+                }
+            }
+        }
+    }
     var towerPosition = (x: 0, y: 0)
     
     var height: Int {
@@ -71,12 +107,32 @@ class Shape {
         return particles[0].count
     }
     
-    init(particles: [[Particle]]) {
+    var description: String {
+        return name
+    }
+    
+    init(name: String, particles: [[Particle]]) {
+        self.name = name
         self.particles = particles
     }
     
+    static func of(kind: Kind) -> Shape {
+        switch kind {
+        case .line:
+            return .makeLine()
+        case .plus:
+            return .makePlus()
+        case .reverseL:
+            return .makeReverseL()
+        case .i:
+            return .makeI()
+        case .block:
+            return .makeBlock()
+        }
+    }
+    
     static func makeLine() -> Shape {
-        return Shape(particles: [
+        return Shape(name: "Line", particles: [
             [
                 Particle(kind: .rock),
                 Particle(kind: .rock),
@@ -87,7 +143,7 @@ class Shape {
     }
     
     static func makePlus() -> Shape {
-        return Shape(particles: [
+        return Shape(name: "Plus", particles: [
             [
                 Particle(kind: .air),
                 Particle(kind: .rock),
@@ -105,7 +161,7 @@ class Shape {
     }
     
     static func makeReverseL() -> Shape {
-        return Shape(particles: [
+        return Shape(name: "Reverse L", particles: [
             [
                 Particle(kind: .air),
                 Particle(kind: .air),
@@ -123,7 +179,7 @@ class Shape {
     }
     
     static func makeI() -> Shape {
-        return Shape(particles: [
+        return Shape(name: "I", particles: [
             [
                 Particle(kind: .rock)
             ], [
@@ -137,7 +193,7 @@ class Shape {
     }
     
     static func makeBlock() -> Shape {
-        return Shape(particles: [
+        return Shape(name: "Block", particles: [
             [
                 Particle(kind: .rock),
                 Particle(kind: .rock),
@@ -156,7 +212,7 @@ class Shape {
 // |  +-------+
 // (0, 0) ->
 
-struct Chamber {
+struct Chamber: CustomStringConvertible {
     var tower = [[Particle]](repeating:
                                 [Particle](repeating:
                                             Particle(kind: .air),
@@ -166,63 +222,313 @@ struct Chamber {
     var tallestY: Int = -1
     var currentlyFallingShape: Shape?
     
+    var description: String {
+        var string = ""
+        var maxI = 0
+        for (i, row) in tower.enumerated() {
+            var rowString = "\(i)\t|"
+            for particle in row {
+                rowString.append(String(describing: particle))
+            }
+            rowString.append("|\n")
+            string = rowString + string
+            maxI = i
+        }
+        let tabs = (maxI / 1000) > 1 ? "\t\t" : "\t"
+        return "\n" + tabs + "|0123456|\n" + string + "\t+-------+"
+    }
+    
+    private mutating func move(shape: Shape, by: (x: Int, y: Int)) {
+        
+    }
+    
     mutating func insert(shape: Shape) {
+        if log {
+            print("Inserting shape=\(shape)")
+        }
         let newHeight = tallestY + 3 + shape.height
-        if tower.count < newHeight {
+        if log {
+            print("  Current tallestY=\(tallestY), newHeight=\(newHeight)")
+        }
+        if newHeight >= tower.count {
+            if log {
+                print("    increasing tower height")
+            }
             tower.append(contentsOf:
                             [[Particle]](repeating:
                                             [Particle](repeating:
                                                         Particle(kind: .air),
                                                        count: 7),
-                                         count: newHeight - tower.count))
+                                         count: newHeight - tower.count + 1))
         }
         
-        for y in zip((tallestY + 3 + 1)..<newHeight, 0..<shape.height) {
-            for x in zip(2..<shape.width, 0..<shape.width) {
+        let towerYMin = tallestY + 3 + 1
+        let towerYMax = newHeight + 1
+        let towerXMin = 2
+        let towerXMax = towerXMin + shape.width
+        
+        if log {
+            print("  Inserting from (\(towerXMin), \(towerYMin)) to (\(towerXMax), \(towerYMax))")
+        }
+        
+        for y in zip(towerYMin..<towerYMax, (0..<shape.height).reversed()) {
+            for x in zip(towerXMin..<towerXMax, 0..<shape.width) {
                 tower[y.0][x.0] = shape.particles[y.1][x.1]
             }
         }
         
-        shape.towerPosition = (x: tallestY + 3 + 1, y: 2)
+        shape.towerPosition = (x: towerXMin, y: newHeight) // top-left of shape
         currentlyFallingShape = shape
+        if log {
+            print("  Setting new towerPosition=\(shape.towerPosition)")
+        }
     }
     
     mutating func process(wind: AirMovement) {
         guard currentlyFallingShape != nil else {
-            print("nothing falling")
+            if log {
+                print("nothing falling")
+            }
             fatalError()
+        }
+        
+        if log {
+            print("Trying to move=\(currentlyFallingShape!)")
         }
         
         switch wind {
         case .left:
+            if log {
+                print("  moving left")
+            }
             // check for wall or rock, don't move if find any in way of rock on left side
-            let leftTop = currentlyFallingShape!.towerPosition.y + 1
-            let leftBottom = leftTop - currentlyFallingShape!.height
+            
+            // these points are aligned to the shape
+            let top = currentlyFallingShape!.towerPosition.y
+            let bottom = top - currentlyFallingShape!.height + 1
             let leftX = currentlyFallingShape!.towerPosition.x
             
-            if leftX - 1 <= 0 {
+            if leftX - 1 < 0 {
+                if log {
+                    print("    cannot move: hit wall")
+                }
                 return
-                
-            } else {
-                for y in zip(leftBottom..<leftTop, 0..<currentlyFallingShape!.height) {
-                    let shapeMaterial = currentlyFallingShape!.particles[y.1][0].kind
-                    let otherMaterial = tower[y.0][leftX - 1].kind
+            }
+            
+            // to see if we can move, go row by row checking for overlap between tower and shape
+            // from the new x coord of the tower up to the x coord one less than shape width, compare the pairs
+            
+            let newXMin = leftX - 1
+            let newXMax = newXMin + currentlyFallingShape!.width
+            
+            for y in zip(bottom..<(top + 1), (0..<currentlyFallingShape!.height).reversed()) {
+                for x in zip(newXMin..<newXMax, 0..<currentlyFallingShape!.width) {
+                    let towerMaterial = tower[y.0][x.0].kind
+                    let shapeMaterial = currentlyFallingShape!.particles[y.1][x.1].kind
                     
-                    if otherMaterial == .rock && shapeMaterial == .rock {
+                    if towerMaterial == .rock && shapeMaterial == .rock {
+                        if log {
+                            print("    cannot move: touching rock")
+                        }
                         return
                     }
                     
-                    if otherMaterial == .air || shapeMaterial == .air {
-                        continue
+                    if shapeMaterial == .rock {
+                        if log {
+                            print("    found shape start, no need to continue in this row")
+                        }
+                        break
                     }
                 }
             }
             
-            // we can move the shape!
             
-            break
+            // we can move the shape!
+            for y in zip(bottom..<(top + 1), (0..<currentlyFallingShape!.height).reversed()) {
+                for x in zip(newXMin..<newXMax, 0..<currentlyFallingShape!.width) {
+                    let currentTowerParticle = tower[y.0][x.0]
+                    let shapeParticle = currentlyFallingShape!.particles[y.1][x.1]
+                    
+                    // only move the rocks
+                    if shapeParticle.kind == .rock {
+                        guard tower[y.0][x.0 + 1] == shapeParticle else {
+                            fatalError()
+                        }
+                        
+                        tower[y.0][x.0] = shapeParticle
+                        tower[y.0][x.0 + 1] = currentTowerParticle
+                    }
+                }
+            }
+            
+            currentlyFallingShape!.towerPosition.x -= 1
+            
         case .right:
-            <#code#>
+            if log {
+                print("  moving right")
+            }
+            // check for wall or rock, don't move if find any in way of rock on right side
+            let top = currentlyFallingShape!.towerPosition.y
+            let bottom = top - currentlyFallingShape!.height + 1
+            let rightX = currentlyFallingShape!.towerPosition.x + currentlyFallingShape!.width - 1
+            
+            if rightX + 1 >= tower[0].count {
+                if log {
+                    print("    cannot move: hit wall")
+                }
+                return
+            }
+                        
+            let newXMax = rightX + 1
+            let newXMin = newXMax - currentlyFallingShape!.width + 1
+            
+            for y in zip(bottom..<(top + 1), (0..<currentlyFallingShape!.height).reversed()) {
+                for x in zip(newXMin..<(newXMax + 1), 0..<currentlyFallingShape!.width).reversed() {
+                    let towerMaterial = tower[y.0][x.0].kind
+                    let shapeMaterial = currentlyFallingShape!.particles[y.1][x.1].kind
+                    
+                    if towerMaterial == .rock && shapeMaterial == .rock {
+                        if log {
+                            print("    cannot move: touching rock")
+                        }
+                        return
+                    }
+                    
+                    if shapeMaterial == .rock {
+                        if log {
+                            print("    found shape start, no need to continue in this row")
+                        }
+                        break
+                    }
+                }
+            }
+            
+            
+            // we can move the shape!
+            for y in zip(bottom..<(top + 1), (0..<currentlyFallingShape!.height).reversed()) {
+                for x in zip(newXMin..<(newXMax + 1), 0..<currentlyFallingShape!.width).reversed() {
+                    let currentTowerParticle = tower[y.0][x.0]
+                    let shapeParticle = currentlyFallingShape!.particles[y.1][x.1]
+                    
+                    // only move the rocks
+                    if shapeParticle.kind == .rock {
+                        guard tower[y.0][x.0 - 1] == shapeParticle else {
+                            fatalError()
+                        }
+                        
+                        tower[y.0][x.0] = shapeParticle
+                        tower[y.0][x.0 - 1] = currentTowerParticle
+                    }
+                }
+            }
+            
+            currentlyFallingShape!.towerPosition.x += 1
         }
     }
+    
+    mutating func processGravity() {
+        guard currentlyFallingShape != nil else {
+            if log {
+                print("nothing falling")
+            }
+            fatalError()
+        }
+        
+        if log {
+            print("Trying to move=\(currentlyFallingShape!) down")
+        }
+        
+        // check for wall or rock, don't move if find any in way of rock on bottom side
+        let left = currentlyFallingShape!.towerPosition.x
+        let right = left + currentlyFallingShape!.width - 1
+        let bottom = currentlyFallingShape!.towerPosition.y - currentlyFallingShape!.height + 1
+        
+        if bottom - 1 < 0 {
+            if log {
+                print("    coming to a rest! hit wall")
+            }
+            currentlyFallingShape!.settled = true
+            tallestY = max(tallestY, currentlyFallingShape!.towerPosition.y)
+            currentlyFallingShape = nil
+            return
+        }
+                
+        let newYMin = bottom - 1
+        let newYMax = newYMin + currentlyFallingShape!.height
+        
+        for x in zip(left..<(right + 1), 0..<currentlyFallingShape!.width) {
+            for y in zip(newYMin..<newYMax, (0..<currentlyFallingShape!.height).reversed()) {
+                let towerMaterial = tower[y.0][x.0].kind
+                let shapeMaterial = currentlyFallingShape!.particles[y.1][x.1].kind
+                
+                if towerMaterial == .rock && shapeMaterial == .rock {
+                    if log {
+                        print("    coming to a rest: touching rock")
+                    }
+                    currentlyFallingShape!.settled = true
+                    tallestY = max(tallestY, currentlyFallingShape!.towerPosition.y)
+                    currentlyFallingShape = nil
+                    return
+                }
+                
+                if shapeMaterial == .rock {
+                    if log {
+                        print("    found shape start, no need to continue in this col")
+                    }
+                    break
+                }
+            }
+        }
+        
+        
+        // we can move the shape!
+        for x in zip(left..<(right + 1), 0..<currentlyFallingShape!.width) {
+            for y in zip(newYMin..<newYMax, (0..<currentlyFallingShape!.height).reversed()) {
+                let currentTowerParticle = tower[y.0][x.0]
+                let shapeParticle = currentlyFallingShape!.particles[y.1][x.1]
+                
+                // only move the rocks
+                if shapeParticle.kind == .rock {
+                    tower[y.0][x.0] = shapeParticle
+                    tower[y.0 + 1][x.0] = currentTowerParticle
+                }
+            }
+        }
+        
+        currentlyFallingShape!.towerPosition.y -= 1
+    }
 }
+
+var chamber = Chamber()
+
+let shapeOrder: [Shape.Kind] = [.line, .plus, .reverseL, .i, .block]
+
+var shapesFallen = 0
+var index = 0
+while shapesFallen < 2023 {
+    if chamber.currentlyFallingShape == nil {
+        let newShape = Shape.of(kind: shapeOrder[shapesFallen % shapeOrder.count])
+        chamber.insert(shape: newShape)
+    }
+
+    let movement = movements[index % movements.count]
+//    print()
+//    print(movement)
+    chamber.process(wind: movement)
+    index += 1
+    
+//    print(chamber)
+
+    chamber.processGravity()
+
+    if chamber.currentlyFallingShape == nil {
+        shapesFallen += 1
+    }
+
+//    print(chamber)
+}
+
+print(chamber)
+print(movements.count)
+print(index)
+print(chamber.tallestY - 1) // index 0 to 1
